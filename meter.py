74731336813a03daa14b0be686425979f14ad22b
Hsieh Chang Wei <jacky790125@gmail.com>
2021-11-19 09:34:09 +0800

    feat: 修正大部份功能

--- meter.py
+++ meter.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-"""
-Script that reads the temperature from a ModBus Temperature sensor connected on /dev/ttyUSB0 on a unix like device via a USB to RS485 converter.
-The minimalmodbus library already return the temperature in an integer in Celcius for this specific sensor model.
-The sensor is a "XY-MD02" model.
-Compatible with Python 3.7
-Install dependencies:
-    pip install minimalmodbus
-Run:
-    python meter.py
-"""
-
-
 import minimalmodbus
 import os
 import time
@@ -18,23 +5,18 @@ import logging
 import requests
 import json
 
+
 def postUrl(info):
     server = os.environ['SERVER']
     url = os.environ['URL']
-    route = server+url
-    requests.post(route,json=json.dumps(info))
+    route = server + url
+    requests.post(route, json=json.dumps(info))
+
 
 # logging
 LOG_LEVEL = os.environ['LOG_LEVEL']
 
 logging.basicConfig(filename='meter.log', level=LOG_LEVEL)
-
-# Meter sensor
-#
-# Meter reading is:
-# Device Address: 0x01
-# Function code:  0x03
-
 
 # Device configuration
 
@@ -43,7 +25,7 @@ DEVICE_ADDRESS = 1
 # ENABLE/DISABLE communication debug mode
 DEVICE_DEBUG = False
 # Master PORT name -- Change as needed for your host.
-PORT_NAME = '/dev/ttyS0'
+PORT_NAME = os.environ['PORT']
 
 # MODBUS instrument initialization
 instrument = minimalmodbus.Instrument(PORT_NAME, DEVICE_ADDRESS, debug=DEVICE_DEBUG)
@@ -52,16 +34,16 @@ instrument = minimalmodbus.Instrument(PORT_NAME, DEVICE_ADDRESS, debug=DEVICE_DE
 # Change as needed depending on your Hardware requirements
 instrument.serial.baudrate = 9600
 instrument.serial.bytesize = 8
-instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
+instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
 instrument.serial.stopbits = 1
 instrument.mode = minimalmodbus.MODE_RTU
 instrument.serial.timeout = 0.2
 
 # Print MODBUS configuration
-print ("MODBUS Configuration\n")
-print ("********************\n")
-print (instrument)
-print ("\n********************\n")
+print("MODBUS Configuration\n")
+print("********************\n")
+print(instrument)
+print("\n********************\n")
 
 # Read Temperature
 REGISTER_ADDRESS_TEMP = 0
@@ -73,28 +55,23 @@ PERIOD = os.environ['PERIOD']
 
 while True:
     # Register number, number of decimals, function code
-    # temperature = instrument.read_register(1, 2, 4)
-    # temperature = instrument.read_register(REGISTER_ADDRESS_TEMP, REGISTER_NUMBER_DECIMALS, ModBus_Command)
-    all = instrument.read_registers(REGISTER_ADDRESS_TEMP, 3, 3)
-    string = time.strftime("%Y-%m-%d %H:%M:%S - ")+ " ".join(map(str, all))
-    print(string)
-    logging.info(string)
-    #try:
-        # result = time.strftime("%Y-%m-%d %H:%M:%S") + " voltage: " + str(temperature) + "\r\n"
-        # print(result)
-    #    logging.info(string)
-    #except IOError:
-    #    print("Failed to read from instrument")
-    #    logging.error("Failed to read from instrument")
-    #count += 1
-    #if count == int(PERIOD):
-    #    info = {
-    #        'voltage': str(temperature)
-    #    }
-        #postUrl(info)
-    #    count = 0
-    time.sleep(1)    
+    try:
+        data = instrument.read_registers(REGISTER_ADDRESS_TEMP, 3, 3)
+        string = time.strftime("%Y-%m-%d %H:%M:%S - ") + " ".join(map(str, data))
+        print(string)
+        logging.info(string)
 
+        count += 1
+        if count == int(PERIOD):
+            info = {
+                'voltage': data[0],
+                'current': data[1],
+                'temperature': data[2]
+            }
+            postUrl(info)
+            count = 0
+    except Exception as e:
+        print(e)
+        logging.debug(e)
 
-
-
+    time.sleep(1)
